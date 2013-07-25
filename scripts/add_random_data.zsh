3107865611a2f7d8fc2339c23e902cd86801d7ec
Meelap Shah <meelapshah@gmail.com>
2013-07-25 17:05:27 +0000

    utility script to quickly add data to kronos
    Reviewers: usmanm, marcua
    
    Reviewed By: usmanm
    
    Differential Revision: http://phabricator.locu.com/D2799

---
+++ scripts/add_random_data.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+KRONOS_URL=http://localhost:8051
+
+KEYS=(apple banana coconut donut fig pear orange kvass grapes tomato strawberry watermelon)
+N=${1:-1}
+
+function add_point() {
+  data=()
+  for key in $KEYS; do
+    data+=( "\"$key\":$(($RANDOM%10))" )
+  done
+  data=${(j:,:)data}
+  data="{\"fruits\":[{${data}}]}"
+  echo "${data}" | lwp-request -m POST "${KRONOS_URL}/1.0/events/put"
+}
+
+repeat $N add_point
+
+# Retrieve stored events
+#echo '{"stream":"fruits","start_time":0,"end_time":24774537120000000}' | lwp-request -m POST ${KRONOS_URL}/1.0/events/get
+
