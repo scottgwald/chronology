893dcc919e2eea7a7b550453be439e3124d565e6
Usman Masood <usmanm@locu.com>
2013-10-29 17:52:28 +0000

    PyKronos Django decorator clean up
    Reviewers: marcua
    
    Reviewed By: marcua
    
    Differential Revision: http://phabricator.locu.com/D3432

--- pykronos/pykronos/utils/django.py
+++ pykronos/pykronos/utils/django.py
@@ -11,6 +11,10 @@ from pykronos.client import KronosClient
 from pykronos.utils.time import kronos_time_now
 
 class KronosLoggingMiddleware(object):
+  FORWARDED_IP_FIELDS = {'HTTP_X_FORWARDED_FOR',
+                         'HTTP_X_FORWARDED_HOST',
+                         'HTTP_X_FORWARDED_SERVER'}
+  
   def __init__(self):
     if not hasattr(settings, 'KRONOS_MIDDLEWARE'):
       raise ImproperlyConfigured
@@ -27,33 +31,39 @@ class KronosLoggingMiddleware(object):
       sleep_block=kronos_config.get('sleep_block', 0.1))
     self.stream_name = kronos_config['stream_name']
     self.namespace = kronos_config.get('namespace')
-    self.log_traceback = kronos_config.get('log_traceback', False)
+    self.log_traceback = kronos_config.get('log_traceback')
+
+  def _get_ip(self, request):
+    if not KronosLoggingMiddleware.FORWARDED_FOR_FIELDS & set(request.META):
+      return request.META.get('REMOTE_ADDR').strip()
+    for field in KronosLoggingMiddleware.FORWARDED_IP_FIELDS:
+      if field in request.META:
+        return request.META[field].split(',')[-1].strip()
   
   def process_request(self, request):
-    request._kronos = {
+    request._kronos_event = {
       'start_time': time.time(),
       '@time': kronos_time_now(),
       'method': request.method,
       'path': request.path,
       'scheme': request.scheme,
-      'query_parameters': request.REQUEST.dict(),
-      'remote_addr': request.META.get('HTTP_X_FORWARDED_FOR',
-                                      request.META['REMOTE_ADDR'])
+      'query_params': request.REQUEST.dict(),
+      'client_ip': self._get_ip(request)
       }
     if request.body:
       try:
-        request._kronos['body'] = json.loads(request.body)
-      except:
-        request._kronos['body'] = request.body
+        request._kronos_event['body'] = json.loads(request.body)
+      except ValueError:
+        request._kronos_event['body'] = request.body
 
   def process_exception(self, request, exception):
     self.client._log_exception(
-      request._kronos, exception,
+      request._kronos_event, exception,
       sys.last_traceback if self.log_traceback else None)
 
   def process_response(self, request, response):
-    start_time = request._kronos.pop('start_time')
-    request._kronos['duration'] = time.time() - start_time
-    self.client.put({self.stream_name: [request._kronos]},
+    start_time = request._kronos_event.pop('start_time')
+    request._kronos_event['duration'] = time.time() - start_time
+    self.client.put({self.stream_name: [request._kronos_event]},
                     namespace=self.namespace)
     return response
