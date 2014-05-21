b19efc250321b0921717243c4600737f8934b7b7
Lydia Gu <lydia@locu.com>
2014-05-21 21:11:01 +0000

    Fix docstring.

--- common/event_tools.py
+++ common/event_tools.py
@@ -1,7 +1,7 @@
 def get_property(event, name):
   """
   Gets a property from `event` using a dot-notation name `name`. It's preferable
-  that dots are not using in key names but this function tries to do the right
+  that dots are not used in key names but this function tries to do the right
   thing by trying all combinations of property names. It prefers property
   names with dots over nested dicts. For example if the dict looks like:
     { 'a': { 'b': 1 },
