cde4bcc2cffd4d4b6b7defe1191d80d8cf88f7f9
Usman Masood <usmanm@locu.com>
2014-06-26 14:48:41 +0000

    Update list_streams to filter based on time it takes to fetch all events in a certain interval
    Test Plan: Manually tested.
    
    Reviewers: marcua
    
    Reviewed By: marcua
    
    Differential Revision: https://phabricator.locu.com/D6825

--- analytics_tools/scripts/list_streams.py
+++ analytics_tools/scripts/list_streams.py
@@ -5,37 +5,59 @@ A script to list the available streams on a Kronos server.
 
 example usage:
   python scripts/list_streams.py --kronos-url http://your-kronos.instance:port [--namespace some_namespace]
+
+  python scripts/list_streams.py --kronos-url http://your-kronos.instance:port [--namespace some_namespace] --start 2014-05-25 --end 2014-05-26 --fetch-timeout 60
+
+  python scripts/list_streams.py --kronos-url http://your-kronos.instance:port [--namespace some_namespace] --read-latency 0.5
 """
+
+import gevent.monkey; gevent.monkey.patch_all()
 
 import argparse
 import logging
-import time
+import os
+import sys
 
-from kronos.utils.math import time_to_kronos_time
+from datetime import datetime
+from gevent import Greenlet
 from pykronos import KronosClient
-from pykronos.errors import KronosClientError
 
 log = logging.getLogger(__name__)
 
-def check_latency(client, namespace, stream, timeout):
-  try:
-    list(client.get(stream,
-                    0,
-                    time_to_kronos_time(time.time()),
-                    limit=1,
-                    namespace=namespace,
-                    timeout=timeout / 1000.0))
-    return True
-  except KronosClientError:
-    return False
+
+def check_stream(client, namespace, stream, start, end, limit, timeout,
+                 latency):
+  def run():
+    for event in client.get(stream, start, end, limit=limit, timeout=latency):
+      # Yeah, I'm useless.
+      pass
+
+  read_greenlet = Greenlet(run)
+  read_greenlet.start()
+  read_greenlet.join(timeout)
+  if not read_greenlet.ready():
+    read_greenlet.kill()
+    success = False
+  else:
+    success = read_greenlet.successful()
+  return success
 
 def main(args):
   client = KronosClient(args.kronos_url)
+  if args.fetch_timeout:
+    start = args.start
+    end = args.end
+    limit = None
+  else:
+    start = 0
+    end = datetime.utcnow()
+    limit = 1000
+
   for stream in client.get_streams(namespace=args.namespace):
-    if args.read_latency is None or check_latency(client,
-                                                  args.namespace,
-                                                  stream,
-                                                  args.read_latency):
+    if not (args.read_latency or args.fetch_timeout):
+      print stream
+    elif check_stream(client, args.namespace, stream, start, end, limit,
+                      args.fetch_timeout, args.read_latency):
       print stream
 
 def process_args():
@@ -46,12 +68,39 @@ def process_args():
   parser.add_argument('--namespace',
                       help='The namespace to read from (optional)')
   parser.add_argument('--read-latency',
-                      type=int,
+                      type=float,
                       help=('Only return streams where read latency is at most '
-                            'this value. Specify in ms. (optional).'))
+                            'this value. Specify in seconds. (optional).'))
+  group = parser.add_argument_group(title='fetch latency arguments',
+                                    description=('Either all of none of these '
+                                                 'args should be present.'))
+  group.add_argument('--start',
+                     help=('When to start retreiving? (format: '
+                           '2003-09-25T10:49:41.5-03:00)'))
+  group.add_argument('--end',
+                     help=('When to end retreiving? (format: '
+                           '2003-09-25T10:49:41.5-03:00)'))
+  group.add_argument('--fetch-timeout',
+                     type=float,
+                     help=('Total time to wait for all events to be fetched '
+                           'between start and end. Only return streams where '
+                           'all events are fetched within timout (in seconds)'))
   args = parser.parse_args()
   return args
 
 
 if __name__ == '__main__':
-  main(process_args())
+  args = process_args()
+
+  if (any((args.start, args.end, args.fetch_timeout)) and
+      not all((args.start, args.end, args.fetch_timeout))):
+    print ('Either all of none of `start`, `end` and `fetch-timeout` should '
+           'be present.')
+    sys.exit(1)
+
+  # TODO(usmanm): Greenlets spit out exception raised within them to stderr,
+  # which pollutes the output in a shell. Redirect stderr to dev/null.
+  devnull = open(os.devnull, 'w')
+  sys.stderr = devnull
+  
+  main(args)
