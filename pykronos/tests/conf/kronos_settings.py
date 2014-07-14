bc49154783c000df1c2aa46787ec6227b8b52545
Usman Masood <usmanm@locu.com>
2014-07-14 19:54:12 +0000

    Fix test settings for pykronos + metis
    Test Plan: -
    
    Reviewers: lydia, dyschultz, marcua
    
    Reviewed By: marcua
    
    Differential Revision: https://phabricator.locu.com/D7021

--- pykronos/tests/conf/kronos_settings.py
+++ pykronos/tests/conf/kronos_settings.py
@@ -6,22 +6,15 @@ from uuid import getnode
 debug = True
 serving_mode = ServingMode.ALL
 
-# Node related settings. `id` should be a unique name that identifies this
-# Kronos instance, while `name` is simply a more human readable name.
 node = {
-  'id':  hex(getnode()), # Unique ID for this Kronos server.
-  'greenlet_pool_size': 20, # Greenlet poolsize per process.
+  'id':  hex(getnode()),
+  'flush_size': 512,
+  'greenlet_pool_size': 20,
+  'gipc_pool_size': 1,
   'log_directory': 'logs',
   'cors_whitelist_domains': map(re.compile, ['localhost'])
 }
 
-# Stream settings.
-# `fields` maps what keys the ID and timestamp should be assigned to in an event
-# JSON.
-# 
-# `format` specifies what a valid stream name looks like. Kronos will use the
-# stream name as part of the key that events are stored under for each backend.
-# Ensure that each backend that you use accepts patterns defined by `format`.
 stream = {
   'format': re.compile(r'^[a-z0-9\_]+(\.[a-z0-9\_]+)*$', re.I)
 }
